e7b0ba079ddc65502a97767ff5c432ecf3b33929
gabrielalves704-blip <gabriel.alves.704@ufrn.edu.br>
2025-09-20 20:37:16 -0300

    Update saveForm.js

--- functions/saveForm.js
+++ functions/saveForm.js
@@ -1,11 +1,16 @@
-fetch("/.netlify/functions/saveForm", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(data)
-})
-.then(res => res.json())
-.then(res => {
-  console.log("Salvo com sucesso:", res);
-  window.location.href = "dados-academicos.html";
-})
-.catch(err => console.error("Erro ao salvar:", err));
+export async function handler(event, context) {
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbzOsK9fWINq8BJ3DHNc_GpnpJ_ErHa33bu6xNXooYjRRMFsyb-LIscrsQ92nGWX1MYu/exec",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: event.body
+    }
+  );
+
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
