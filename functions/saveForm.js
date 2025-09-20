87e65685278ad8308521a78ca7fb077c0090b684
gabrielalves704-blip <gabriel.alves.704@ufrn.edu.br>
2025-09-20 20:44:53 -0300

    Update saveForm.js

--- functions/saveForm.js
+++ functions/saveForm.js
@@ -1,16 +1,26 @@
 export async function handler(event, context) {
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbzOsK9fWINq8BJ3DHNc_GpnpJ_ErHa33bu6xNXooYjRRMFsyb-LIscrsQ92nGWX1MYu/exec",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: event.body
-    }
-  );
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzOsK9fWINq8BJ3DHNc_GpnpJ_ErHa33bu6xNXooYjRRMFsyb-LIscrsQ92nGWX1MYu/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: event.body
+      }
+    );
 
-  const data = await response.json();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 }
+
+
